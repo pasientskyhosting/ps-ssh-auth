950e74fec6bde607ca0fb301391d5ea4fe8ff326
Andreas Kruger <ak@patientsky.com>
2017-11-07 20:08:50 +0100

    Updated to first working version

--- ssh-auth.go
+++ ssh-auth.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"github.com/ddliu/go-httpclient"
 	"github.com/tidwall/gjson"
 	"os"
@@ -13,6 +14,8 @@ var (
 	hostname   string
 	user       string
 	auth_token string
+	login      string
+	pass       string
 )
 
 func isJSON(s string) bool {
@@ -25,40 +28,41 @@ func init() {
 	// Get hostname of server automatically
 	name, err := os.Hostname()
 	if err != nil {
-		println("Cannot get hostname of server. Please use the flag")
+		fmt.Print("Cannot get hostname of server. Please use the flag")
 	}
 
 	// Define inputs
 	flag.StringVar(&server, "server", "http://127.0.0.1:5000", "URL to PrivacyIDEA server.")
 	flag.StringVar(&hostname, "hostname", name, "Hostname of server to validate")
 	flag.StringVar(&user, "user", "", "Username to validate")
+	flag.StringVar(&login, "login", "admin", "Login username to PrivacyIDEA")
+	flag.StringVar(&pass, "pass", "test", "Login password to PrivacyIDEA")
 	flag.Parse()
 }
 
 func main() {
 	httpclient.Defaults(httpclient.Map{
 		httpclient.OPT_USERAGENT: "SSH-Auth",
-		"Accept-Language":        "en-us",
 	})
 
 	res, err := httpclient.
 		Begin().
 		WithOption(httpclient.OPT_TIMEOUT, 10).
 		Post(server+"/auth", map[string]string{
-			"username": "admin",
-			"password": "test",
+			"username": login,
+			"password": pass,
 		})
 
 	if err != nil {
-		println(err)
-		os.Exit(0)
+		fmt.Print(err)
+		os.Exit(1)
 	}
 
 	result, err := res.ToString()
 
 	if isJSON(result) != true {
-		println("Did not get a JSON response")
-		os.Exit(0)
+		fmt.Print("Did not get a JSON response")
+		os.Exit(2)
 	}
 
 	auth_token := gjson.Get(result, "result.value.token")
@@ -73,22 +77,22 @@ func main() {
 			})
 
 		if err != nil {
-			println(err)
-			os.Exit(0)
+			fmt.Print(err)
+			os.Exit(3)
 		}
 
 		result, err := res.ToString()
 
 		if isJSON(result) != true {
-			println("Did not get a JSON response")
-			os.Exit(0)
+			fmt.Print("Did not get a JSON response")
+			os.Exit(4)
 		}
 
 		keys := gjson.Get(result, "result.value.ssh")
 		keys.ForEach(func(key, value gjson.Result) bool {
 			ssh_key := gjson.Get(value.String(), "sshkey")
-			println(ssh_key.String())
-			return true // keep iterating
+			fmt.Print(ssh_key.String())
+			return true
 		})
 	}
 }
